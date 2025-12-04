aa735cba4185939c93850fcffbf5974f250d1c70
Papawarin <review3848@gmail.com>
2025-12-05 00:22:10 +0700

    Update student-scan.js

--- js/student-scan.js
+++ js/student-scan.js
@@ -194,7 +194,7 @@ async function loadHistory() {
   try {
     const res = await fetch(API_BASE, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "text/plain;charset=utf-8" },
       body: JSON.stringify({
         action: "getStudentHistory",
         studentId: student.id
