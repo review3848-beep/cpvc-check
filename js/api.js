46fa38614b9af5a4d9f08af19762516d7deb79eb
Papawarin <review3848@gmail.com>
2025-12-09 20:32:51 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,22 +1,16 @@
-// js/api.js
-// ⚠️ อย่าลืมตรวจสอบ URL นี้ว่าเป็นอันล่าสุดที่คุณ Deploy หรือยังนะครับ
 export const API_BASE = "https://script.google.com/macros/s/AKfycbxXZy_ebPiGJEDMa2Q7EPfaTCE5RkSe7OnlZ_et3-HtR74uGP1EVigXjbbucDJBRSqRTw/exec";
-
 export async function callApi(action, payload = {}) {
   try {
     const res = await fetch(API_BASE, {
       method: "POST",
-      // ✅ เปลี่ยน header เป็น text/plain เพื่อแก้ CORS Error 100%
       headers: { "Content-Type": "text/plain;charset=utf-8" }, 
       body: JSON.stringify({
         action: action,
         ...payload
       })
     });
-
     const data = await res.json();
     return data;
-
   } catch (err) {
     console.error("API Error:", err);
     return { success: false, message: "ไม่สามารถเชื่อมต่อเซิร์ฟเวอร์ได้" };
