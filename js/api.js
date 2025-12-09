cb0811fdf88e2ef5cee053592eb2190022d5f423
Papawarin <review3848@gmail.com>
2025-12-09 14:32:03 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 // js/api.js
 // ⚠️ อย่าลืมตรวจสอบ URL นี้ว่าเป็นอันล่าสุดที่คุณ Deploy หรือยังนะครับ
-export const API_BASE = "https://script.google.com/macros/s/AKfycbzcTUMzFL3cX8t0cYgIugKMr5O5uvrhVzuOc-bnvuS8Z9HZ7Vu5FhU9CzSwQLk46Dp7Og/exec";
+export const API_BASE = "https://script.google.com/macros/s/AKfycbyZuvlYUC_AaSBX32QFONuVIQux-y5aUf40WPTunDSfQu0wUA9_390QhWmTW_fL6gm8gg/exec";
 
 export async function callApi(action, payload = {}) {
   try {
