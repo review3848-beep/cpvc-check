c201b8bd39708e5ff1fdc7392f1760f4b511e8ee
Papawarin <review3848@gmail.com>
2026-01-05 09:32:34 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-export const API_BASE = "https://script.google.com/macros/s/AKfycbwPXhI-rDvG42gAkvpoyOXYiteBI_YBxrj_TiHD5E9CwpPt082t6dOgGF9WpCUal6ZkkQ/exec";
+export const API_BASE = "https://script.google.com/macros/s/AKfycbxfPE2-GzbzDKF5kzNV3gaqk2ibitwfBucTe-b2oGu493pao-DKh5lvbLtDrjexOmj6_Q/exec";
 
 export async function callApi(action, payload = {}) {
   const res = await fetch(API_BASE, {
