0ab51efd5146d80b286f4306de136e4ab8d24c17
Papawarin <review3848@gmail.com>
2026-01-12 23:18:29 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 //api.js
 export const API_BASE =
-  "https://script.google.com/macros/s/AKfycbyA6Uu6LdjdqYlyMHnI9ksFUA-lipprSXJ2yozP99FBsa8CUN-yXNUFT6eLMdfrAc8XLQ/exec";
+  "https://script.google.com/macros/s/AKfycbxY6R53PUY3EXfmJlpBR2Iz3-VGslwA6RsBNJzIhX6l445x3F5qeD0rOmxMPz8YZFesag/exec";
 
 /* ================= API CALL ================= */
 export async function callApi(action, params = {}) {
